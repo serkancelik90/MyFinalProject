765f0a2d54e933e89d921b77c70620789bd4a0fc
Serkan Çelik <serkancelik9090@hotmail.com>
2021-02-03 23:47:33 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //SOLID 
+        //Open Closed Principle (Yaptığın yazılıma yeni bir özellik ekliyorsan, mevcuttaki hiçbir koduna dokunamazsın.)
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName + product.UnitPrice);
+            }
+        }
+    }
+}
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint (generic kısıt)
+    //class: referans tip olabilir demek
+    //herhangi bi class yazamaması için IEntity olmasını belirtmek gerek.Ya da IEntity olan bir class olabilir.
+    //new diyerek newlenebilir olmalı demiş oluyoruz. böylece soyut nesneyi yazamıyoruz
+    public interface IEntityRepository<T> where T: class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
